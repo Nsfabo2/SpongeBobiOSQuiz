e4d6775e0b674cca2c6d4d99c2e8e7131925756f
Najla Abothnain <na.abothnain@gmail.com>
2021-12-07 11:39:57 +0300

    Initial Commit

---
+++ SpongeBobiOSQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpongeBobiOSQuiz
+//
+//  Created by Najla on 03/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
